39a047a0c4ef7733f3bbd7f5d58a58f78477e9fd
Jinsoo Yuk <csdbest12121@gmail.com>
2024-02-14 02:07:34 +0900

    first commit

--- main.js
+++ main.js
@@ -16,8 +16,20 @@ function addTask() {
     let taskContent = taskInput.value
     taskList.push(taskContent)
     console.log(taskList)
+    render()
 }
 
 function render() {
-    let
+    let resultHTML = '';
+    for(let i=0;i<taskList.length;i++) {
+        resultHTML += `<div class="task">
+        <div>${taskList[i]}</div>
+        <div>
+            <button>Check</button>
+            <button>Delete</button>
+        </div>
+    </div>`;
+    }
+
+    document.getElementById('task-board').innerHTML = resultHTML;
 }
